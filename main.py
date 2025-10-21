34c3e71c279cf82b6e3f23224bf1c59081809151
Vuduweb <info@vu-du-web.com>
2025-10-21 13:56:56 +0200

    Fix indentation for success message in main.py

--- main.py
+++ main.py
@@ -930,9 +930,8 @@ with tabs[0]:
                     
                     progress_bar.progress((i + 1) / len(st.session_state.paa_selected))
                     time.sleep(0.5)
-                
-                st.success("Articles générés !")
-                st.session_state.paa_selected = []
+                        st.success("Articles générés !")
+                        st.session_state.paa_selected = []
                 st.rerun()
     
     if st.session_state.paa_content_generated:
